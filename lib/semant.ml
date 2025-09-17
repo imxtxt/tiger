495e0391dfe30c00fc4c083c90f1b37c87826150
illAlwayswatching <2104962413@qq.com>
2025-09-17 14:47:18 +0800

    1

--- lib/semant.ml
+++ lib/semant.ml
@@ -23,6 +23,18 @@ let rec check_comp (t1 : Types.t) (t2 : Types.t) =
   | Name (_, { contents = Some t }), t2 -> check_comp t1 t2
   | t1, Name (_, { contents = Some t2 }) -> check_comp t1 t2
 
+let rec check_equal (t1 : Types.t) (t2 : Types.t) =
+  match t1, t2 with
+  | Int, Int
+  | String, String
+  | Unit, Unit
+  | Nil, Record _
+  | Record _, Nil -> ()
+  | Record (_, id1), Record (_, id2) when id1 = id2 -> ()
+  | Array (_, id1), Array (_, id2) when id1 = id2 -> ()
+  | Name (_, { contents = Some t }), t2 -> check_equal t1 t2
+  | t1, Name (_, { contents = Some t2 }) -> check_equal t1 t2
+
 let rec trans_var (venv : venv) (tenv : tenv) (var : Ast.var) : stms_exp_ty =
   match var with
   | Simple id ->
@@ -71,10 +83,16 @@ and trans_exp (venv : venv) (tenv : tenv) (exp : Ast.exp) : stms_exp_ty =
   | Record (t, inits) -> 
       let Record (fields, id) = SMap.find t tenv in
       let inits_stmss_exps_tys = List.map (fun (n, e) -> n, trexp e) inits in
-      List.iter2 (fun (n1, a) (n2, t)-> check_comp a.ty t) inits_stmss_exps_tys fields ;
+      List.iter2 
+        (fun (n1, {ty = t1; _}) (n2, t2)-> 
+          if n1 = n2 then 
+            check_comp t1 t2
+          else
+            assert false)
+        inits_stmss_exps_tys fields ;
       { stms = []; exp = (); ty = Record (fields, id) }
   | Seq [] -> { stms = []; exp = (); ty = Unit }
-  | Seq (e1 :: es) -> 
+  | Seq (e1 :: es) ->
       List.fold_left
         (fun acc e -> trexp e)
         (trexp e1) es
@@ -91,6 +109,7 @@ and trans_exp (venv : venv) (tenv : tenv) (exp : Ast.exp) : stms_exp_ty =
       let { stms; exp; ty = Int } = trexp e1 in
       let { stms; exp; ty = e2_ty } = trexp e2 in
       let { stms; exp; ty = e3_ty } = trexp e3 in
+      check_equal e2_ty e3_ty ;
       { stms; exp; ty = e2_ty }
   | While (e1, e2) ->
       let { stms; exp; ty = Int } = trexp e1 in
@@ -107,3 +126,7 @@ and trans_exp (venv : venv) (tenv : tenv) (exp : Ast.exp) : stms_exp_ty =
 
 [@@@warning "+partial-match"]
 [@@@warning "+unused-var-strict"]
+
+let trans_prog (exp : Ast.exp) : unit =
+  trans_exp Env.base_venv Env.base_tenv exp
+  |> ignore
